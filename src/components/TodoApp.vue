75a893da06940c0d27b395b23ca2b2a93f355756
José David Rodriguez Payo <josedavidrp12@gmail.com>
2025-06-11 20:08:30 +0200

    Puesta a punto para proyecto

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="todo-app">
+    <h1>Lista de tareas</h1>
+    <todo-form />
+    <todo-list />
+  </div>
+</template>
+
+<script setup>
+import TodoForm from '@/components/TodoForm.vue'
+import TodoList from '@/components/TodoList.vue'
+</script>
+
+<style scoped></style>
