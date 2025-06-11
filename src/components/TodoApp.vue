79d3e936ad870878d34715573928864919c5b38d
José David Rodriguez Payo <josedavidrp12@gmail.com>
2025-06-11 20:37:15 +0200

    Feature: Eliminar tareas de array. Estilo: Añadido CSS a todos los componentes

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -11,4 +11,11 @@ import TodoForm from '@/components/TodoForm.vue'
 import TodoList from '@/components/TodoList.vue'
 </script>
 
-<style scoped></style>
+<style scoped>
+.todo-app {
+  padding: 5vh;
+  min-height: 100vh;
+  width: 30vw;
+  background-color: #f7f9fc;
+}
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
     <div v-for="todo in todoList" :key="todo.id" class="item">
-      <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
-      <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+      <div class="content">
+        <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
+        <div>
+          <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+          <span @click="deleteTodo(todo.id)" class="x">&#10060;</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -14,10 +19,25 @@ import { storeToRefs } from 'pinia'
 const store = useTodoListStore()
 
 const { todoList } = storeToRefs(store)
-const { toggleCompleted } = store
+const { toggleCompleted, deleteTodo } = store
 </script>
 
 <style scoped>
+span {
+  margin: 0 10px;
+  cursor: pointer;
+}
+.item {
+  display: flex;
+  justify-content: center;
+}
+.content {
+  display: flex;
+  font-size: 1.5em;
+  justify-content: space-between;
+  width: 80vw;
+  padding: 5px;
+}
 .completed {
   text-decoration: line-through;
 }
